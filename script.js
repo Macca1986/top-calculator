d836e342222a9485b806633c4c5d873c8282e771
Macca1986 <matthew_mack86@hotmail.com>
2025-02-27 14:50:30 +0400

    Create JS functions to calculations
    Created the functions in JS to conduct calculations when required.
    Initialized variables to store numbers and operators.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Hello world")
+// functions for basic calculator operations
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// variables for storing values and operators
+let firstNum;
+let secondNum;
+let operator;
+
+// operate function to take two nums and operator and call one of the basic functions
+function operate (operator, num1, num2) {
+    // can I make this cleaner and just use the operator parameter as the function name?
+    if (operator == "add") {
+        return add(num1, num2);
+    } else if (operator == "subtract") {
+        return subtract(num1, num2);
+    } else if (operator == "multiply") {
+        return multiply(num1, num2);
+    } else if (operator == "divide") {
+        return divide(num1, num2);
+    }
+}
